7476f8369c9690064311c22afe431c7e92bd48dc
etrobot <etrobot@outlook.com>
2020-11-09 23:36:48 +0800

    remove some bak action

--- Bash/push.sh
+++ Bash/push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#cd /Users/admin/Documents/DEV/PY/html
-#git checkout --orphan newBranch
-#git add -A  # Add all files and commit them
-#git commit -m "Removed history, due to large data"
-#git branch -D master  # Deletes the master branch
-#git branch -m master  # Rename the current branch to master
-#git push -f origin master  # Force push master branch to github
-#git gc --aggressive --prune=all     # remove the old files
+cd /Users/admin/Documents/DEV/PY/html
+git checkout --orphan newBranch
+git add -A  # Add all files and commit them
+git commit -m "Removed history, due to large data"
+git branch -D master  # Deletes the master branch
+git branch -m master  # Rename the current branch to master
+git push -f origin master  # Force push master branch to github
+git gc --aggressive --prune=all     # remove the old files
 
 rm /Users/admin/Documents/DEV/PY/upknow/*.*
 cp -v /Users/admin/Documents/DEV/PY/html/*.* /Users/admin/Documents/DEV/PY/upknow
