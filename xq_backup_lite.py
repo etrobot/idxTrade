62cd5e32d0efec34c5ff715b80cbe0084e708be6
etrobot <etrobot@outlook.com>
2020-11-10 19:29:26 +0800

    update

--- xq_backup_lite.py
+++ xq_backup_lite.py
@@ -335,7 +335,7 @@ def dailyCheck(mkt=None,pdate=None,test=0):
     mtmDfBAK=indDf[list(cal.keys())].copy()
     mtmDfBAK.to_csv('md/'+mkt+pdate.strftime('%Y%m%d')+'.txt',encoding=ENCODE_IN_USE,index_label='symbol')
 
-    if mkt=='cn' and len(sys.argv)==0:
+    if len(sys.argv)==0:
         idxtrade=idxTrade('cn',0)
         idxtrade.run()
 
