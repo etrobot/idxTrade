58c3d45270ac3cfbd99ef2097f730e3e1c2d6b86
franklin <etrobot@outlook.com>
2020-11-13 07:53:28 +0800

    update

--- Bash/push.sh
+++ Bash/push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /Users/admin/Documents/DEV/PY/upknow
-rm -f *.png
 git checkout --orphan newBranch
 git add -A  # Add all files and commit them
 git commit -m "Removed history, due to large data"
@@ -8,6 +7,7 @@ git branch -D master  # Deletes the master branch
 git branch -m master  # Rename the current branch to master
 git push -f origin master  # Force push master branch to github
 git gc --aggressive --prune=all     # remove the old files
+
 /usr/local/bin/node /Users/admin/Documents/DEV/PY/idxTrade/gitee.js
 
 cd /Users/admin/Documents/DEV/PY/html
