eb1febf5429e3a8bb6d3f33e11d52fc48aa207ac
franklin <etrobot@outlook.com>
2020-11-14 20:20:11 +0800

    update

--- Bash/push.sh
+++ Bash/push.sh
@@ -2,14 +2,13 @@
 
 gitPush(){
 git checkout --orphan newBranch
-#git merge -s ours master
 git add -A  # Add all files and commit them
 git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git branch -D master  # Deletes the master branch
 git branch -m master  # Rename the current branch to master
 git push -f origin master  # Force push master branch to github
-git reflog expire --expire=now --all
-git gc --aggressive --prune=now # remove the old files
+git reflog expire --expire=now --all # remove the old files 1
+git gc --aggressive --prune=now # remove the old files 2
 }
 
 cd "$(dirname "$0")" || exit
