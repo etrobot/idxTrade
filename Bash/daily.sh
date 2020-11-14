35628a8147ae3f5e258c29595d6fa683251300ce
franklin <etrobot@outlook.com>
2020-11-14 13:39:33 +0800

    update

--- Bash/daily.sh
+++ Bash/daily.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-rm -f ../../upknow/`date +%w`/$1/*.png
+
+if $1=="us"; then
+  rm -f ../../upknow/`date -v-1d +%w `/$1/*.png
+else
+  rm -f ../../upknow/`date +%w `/$1/*.png
+fi
 
 cd .. && /usr/local/bin/python3.7 -u xq_backup_lite.py $1 $2;
 
--- Bash/push.sh
+++ Bash/push.sh
@@ -2,22 +2,24 @@
 cd "$(dirname "$0")"
 cd ../../upknow
 git checkout --orphan newBranch
-git merge -s ours master
+#git merge -s ours master
 git add -A  # Add all files and commit them
 git commit -m "Removed history, due to large data"
 git branch -D master  # Deletes the master branch
 git branch -m master  # Rename the current branch to master
 git push -f origin master  # Force push master branch to github
-git gc --aggressive --prune=all     # remove the old files
+git reflog expire --expire=now --all
+git gc --aggressive --prune=now # remove the old files
 
 /usr/local/bin/node ../gitee.js
 
 cd ../html
 git checkout --orphan newBranch
-git merge -s ours master
+#git merge -s ours master
 git add -A  # Add all files and commit them
 git commit -m "Removed history, due to large data"
 git branch -D master  # Deletes the master branch
 git branch -m master  # Rename the current branch to master
 git push -f origin master  # Force push master branch to github
-git gc --aggressive --prune=all     # remove the old files
+git reflog expire --expire=now --all
+git gc --aggressive --prune=now     # remove the old files
