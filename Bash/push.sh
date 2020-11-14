9c127d73b2ecbc2acf5ce4bc740dca56d74ceac2
franklin <etrobot@outlook.com>
2020-11-14 14:45:55 +0800

    update

--- Bash/push.sh
+++ Bash/push.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-cd ../../upknow
+
+gitPush(){
 git checkout --orphan newBranch
 #git merge -s ours master
 git add -A  # Add all files and commit them
-git commit -m "Removed history, due to large data"
+git commit -m $(date "+%Y-%m-%d %H:%M:%S")
 git branch -D master  # Deletes the master branch
 git branch -m master  # Rename the current branch to master
 git push -f origin master  # Force push master branch to github
 git reflog expire --expire=now --all
 git gc --aggressive --prune=now # remove the old files
+}
 
+cd "$(dirname "$0")"
+cd ../../upknow
+gitPush
 /usr/local/bin/node ../gitee.js
-
 cd ../html
-git checkout --orphan newBranch
-#git merge -s ours master
-git add -A  # Add all files and commit them
-git commit -m "Removed history, due to large data"
-git branch -D master  # Deletes the master branch
-git branch -m master  # Rename the current branch to master
-git push -f origin master  # Force push master branch to github
-git reflog expire --expire=now --all
-git gc --aggressive --prune=now     # remove the old files
+gitPush
