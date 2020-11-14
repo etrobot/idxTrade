c817cd8faa5040a066d8d97e4311ccd25997bcc5
franklin <etrobot@outlook.com>
2020-11-14 12:53:30 +0800

    update

--- Bash/push.sh
+++ Bash/push.sh
@@ -9,7 +9,7 @@ git branch -m master  # Rename the current branch to master
 git push -f origin master  # Force push master branch to github
 git gc --aggressive --prune=all     # remove the old files
 
-/usr/local/bin/node ../../gitee.js
+/usr/local/bin/node ../gitee.js
 
 cd ../html
 git fetch
