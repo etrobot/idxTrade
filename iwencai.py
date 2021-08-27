ea03e2ee0f979f3bf36097540e18d4afe3e14671
etrobot <etrobot@outlook.com>
2021-08-27 14:39:31 +0800

    wencai

--- iwencai.py
+++ iwencai.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
         df['5日均线']=pd.to_numeric(df['5日均线'], errors='coerce')
         df['最新涨跌幅']=pd.to_numeric(df['最新涨跌幅'], errors='coerce')
         df['区间涨跌幅:前复权'] = np.round(pd.to_numeric(df["区间涨跌幅:前复权"], errors='coerce'),2)
-        df['factor']=(df['区间涨跌幅:前复权']+df['最新涨跌幅'])*df['5日均线']/df['最新价']
+        df['factor']=df['区间涨跌幅:前复权']*df['5日均线']/df['最新价']
         df['date'] = idx.index[-1]
         df['type'] = k[1:]
         wencaiDf = wencaiDf.append(df[['股票简称', '股票代码','最新涨跌幅', '区间涨跌幅:前复权','factor','date','type']])
