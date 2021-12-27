4f43c1409fe070d76766f0da1f95663157bb2dc8
etrobot <etrobot@outlook.com>
2021-12-27 14:30:29 +0800

    update

--- iwencai.py
+++ iwencai.py
@@ -93,11 +93,11 @@ if __name__ == "__main__":
         t.sleep(10*(int(list(conf['wencai'].keys()).index(k)!=0)))
         df = crawl_data_from_wencai(q)
         df.to_csv('test.csv',encoding='GBK')
-        print(df.columns)
+        # print(df.columns)
         df['code']=df['股票代码'].str[:6]
         df['股票代码'] = df['股票代码'].str[7:] + df['股票代码'].str[:6]
         df['a股市值(不含限售股)']= np.round(pd.to_numeric(df['a股市值(不含限售股)'], errors='coerce')/1000000000)*10
-        df['factor']= pd.to_numeric(df['涨停价'], errors='coerce')/pd.to_numeric(df["42日均线"], errors='coerce')
+        df['factor']= pd.to_numeric(df['涨停价'], errors='coerce')/pd.to_numeric(df["20日均线"], errors='coerce')
         df['date'] = idx.index[-1]
         df['type'] = k[1:]
         wencaiDf = wencaiDf.append(df)
