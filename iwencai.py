f9412ffefe0a28027bd561318e83a3257a27de18
tfproduct01 <etrobot@outlook.com>
2022-07-29 12:27:54 +0800

    wencai

--- iwencai.py
+++ iwencai.py
@@ -96,8 +96,8 @@ if __name__ == "__main__":
         df['code']=df['股票代码'].str[:6]
         df['股票代码'] = df['股票代码'].str[7:] + df['股票代码'].str[:6]
         df['a股市值(不含限售股)']= np.round(pd.to_numeric(df['a股市值(不含限售股)'], errors='coerce')/1000000000)*10
-        # df['factor']= pd.to_numeric(df["20日均线"], errors='coerce')/pd.to_numeric(df['跌停价'], errors='coerce')*(100+pd.to_numeric(df['最新涨跌幅'], errors='coerce'))
-        df['factor']= df['融资余额增速']
+        df['factor']= pd.to_numeric(df["5日均线"], errors='coerce')/pd.to_numeric(df['跌停价'], errors='coerce')*(100+pd.to_numeric(df['最新涨跌幅'], errors='coerce'))
+        # df['factor']= df['融资余额增速']
         df['date'] = idx.index[-1]
         df['type'] = k[1:]
         if os.path.isfile('limit/limits.csv'):
