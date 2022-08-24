fed1fe649807a5d20d26d664ba517a43af1eb867
tfproduct01 <etrobot@outlook.com>
2022-08-24 18:23:14 +0800

    video, use azure tts

--- video.py
+++ video.py
@@ -19,13 +19,14 @@ from urllib.parse import quote_plus
 
 import cv2
 from tqdm import tqdm
-from mutagen.mp3 import MP3
 from moviepy.editor import VideoFileClip, AudioFileClip,concatenate_videoclips
 
 from XueqiuPortfolio import *
 from iwencai import crawl_data_from_wencai
 from QuotaUtilities import renderHtml
 from translate import *
+import mutagen
+import azure.cognitiveservices.speech as speechsdk
 
 FOLDER='video/'
 MAXHOLDING=4
@@ -33,104 +34,23 @@ MAXHOLDING=4
 IS_PY3 = sys.version_info.major == 3
 conf = configparser.ConfigParser()
 conf.read('config.ini')
-API_KEY = conf['baidu']['API_KEY']
-SECRET_KEY = conf['baidu']['SECRET_KEY']
-
-
-# 发音人选择, 基础音库：0为度小美，1为度小宇，3为度逍遥，4为度丫丫，
-# 精品音库：5为度小娇，103为度米朵，106为度博文，110为度小童，111为度小萌，默认为度小美
-PER = 0
-# 语速，取值0-15，默认为5中语速
-SPD = 6
-# 音调，取值0-15，默认为5中语调
-PIT = 2
-# 音量，取值0-9，默认为5中音量
-VOL = 5
-# 下载的文件格式, 3：mp3(default) 4： pcm-16k 5： pcm-8k 6. wav
-AUE = 3
-
-FORMATS = {3: "mp3", 4: "pcm", 5: "pcm", 6: "wav"}
-FORMAT = FORMATS[AUE]
-
-CUID = "123456PYTHON"
-
-TTS_URL = 'http://tsn.baidu.com/text2audio'
-
-"""  TOKEN start """
-
-TOKEN_URL = 'http://aip.baidubce.com/oauth/2.0/token'
-SCOPE = 'audio_tts_post'  # 有此scope表示有tts能力，没有请在网页里勾选
-
-
-class DemoError(Exception):
-    pass
-
-def fetch_token():
-    print("fetch token begin")
-    params = {'grant_type': 'client_credentials',
-              'client_id': API_KEY,
-              'client_secret': SECRET_KEY}
-    post_data = urlencode(params)
-    if (IS_PY3):
-        post_data = post_data.encode('utf-8')
-    req = Request(TOKEN_URL, post_data)
-    try:
-        f = urlopen(req, timeout=5)
-        result_str = f.read()
-    except URLError as err:
-        print('token http response http code : ' + str(err.code))
-        result_str = err.read()
-    if (IS_PY3):
-        result_str = result_str.decode()
-
-    print(result_str)
-    result = json.loads(result_str)
-    print(result)
-    if ('access_token' in result.keys() and 'scope' in result.keys()):
-        if not SCOPE in result['scope'].split(' '):
-            raise DemoError('scope is not correct')
-        print('SUCCESS WITH TOKEN: %s ; EXPIRES IN SECONDS: %s' % (result['access_token'], result['expires_in']))
-        return result['access_token']
-    else:
-        raise DemoError('MAYBE API_KEY or SECRET_KEY not correct: access_token or scope not found in token response')
-"""  TOKEN end """
-
 
 def text2voice(text:str,audioFile='result'):
-    token = fetch_token()
-    tex = quote_plus(text)  # 此处TEXT需要两次urlencode
-    print(tex)
-    params = {'tok': token, 'tex': tex, 'per': PER, 'spd': SPD, 'pit': PIT, 'vol': VOL, 'aue': AUE, 'cuid': CUID,
-              'lan': 'zh', 'ctp': 1}  # lan ctp 固定参数
+    filename=FOLDER + audioFile + '.wav'
+    speech_config = speechsdk.SpeechConfig(subscription="b761438d396d48c585fa680d5d3575b1", region="eastasia")
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=filename)
 
-    data = urlencode(params)
-    print('test on Web Browser' + TTS_URL + '?' + data)
+    # The language of the voice that speaks.
+    speech_config.speech_synthesis_voice_name = 'zh-CN-XiaoyanNeural'
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-    req = Request(TTS_URL, data.encode('utf-8'))
-    has_error = False
-    try:
-        f = urlopen(req)
-        result_str = f.read()
-
-        headers = dict((name.lower(), value) for name, value in f.headers.items())
-
-        has_error = ('content-type' not in headers.keys() or headers['content-type'].find('audio/') < 0)
-    except  URLError as err:
-        print('asr http response http code : ' + str(err.code))
-        result_str = err.read()
-        has_error = True
-
-    save_file = FOLDER+"error.txt" if has_error else FOLDER+audioFile+'.' + FORMAT
-    with open(save_file, 'wb') as of:
-        of.write(result_str)
-
-    if has_error:
-        if (IS_PY3):
-            result_str = str(result_str, 'utf-8')
-        print("tts api  error:" + result_str)
-
-    print("result saved as :" + save_file)
-    return FOLDER+audioFile+'.' + FORMAT
+    result = speech_synthesizer.speak_text_async(text).get()
+    # Check result
+    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("Speech synthesized to speaker for text [{}]".format(text))
+        stream = speechsdk.AudioDataStream(result)
+        stream.save_to_wav_file(filename)
+    return filename
 
 def is_contain_chinese(check_str):
     for ch in check_str:
@@ -143,14 +63,14 @@ def getSinaNews(symbol:str):
     print(url)
     resp=requests.get(url=url, headers={"user-agent": "Mozilla"})
     resp.encoding = 'GB18030'
-    html = etree.HTML(resp.text.replace(' | ','|').replace('.US',''))
+    html = etree.HTML(resp.text.replace(' | ','|').replace('.US','').replace('data-jzz-gui-player="true"></a>','data-jzz-gui-player="true">Nan</a>'))
     df=pd.DataFrame()
     df['title'] = html.xpath('//ul[@class="xb_list"]//a/text()')
     df['url'] = html.xpath('//ul[@class="xb_list"]//a/@href')
     dateTxt= html.xpath('//ul[@class="xb_list"]//span[@class="xb_list_r"]/text()')
     df['dateText'] =dateTxt
     df['date']=[datetime.strptime(x.split('|')[1],"%Y年%m月%d日 %H:%S")  for x in dateTxt]
-    df=df[~df['title'].str.contains("美股", na=False)]
+    df=df[~df['title'].str.contains("美股|Nan", na=False)]
     df = df[~df['dateText'].str.contains("全景网动态|环球市场播报|环球网快看", na=False)]
     df = df[df['date']>datetime.now()-timedelta(days=180)]
     # df = df[df['url'].str.contains("2022", na=False)]
@@ -173,13 +93,13 @@ def get_video(count:int,symbol:str,videoFile=FOLDER + 'video.mp4'):
 # 加入音频
 def get_audio(symbol:str,videoFile=FOLDER+'video.mp4'):
     video = VideoFileClip(videoFile)
-    videos = video.set_audio(AudioFileClip(FOLDER+symbol+'.mp3'))  # 音频文件
+    videos = video.set_audio(AudioFileClip(FOLDER+symbol+'.wav'))  # 音频文件
     videos.write_videofile(FOLDER+symbol+'.mp4')  # 保存合成视频
 
 
 # 计算每个音频的时间（秒）
 def get_time_count(audioFile='result'):
-    audio = MP3(FOLDER+audioFile+".mp3")
+    audio = mutagen.File(FOLDER+audioFile+".wav")
     time_count = int(audio.info.length)
     return time_count
 
@@ -305,16 +225,17 @@ def genTradeVideo(tradeDate:datetime,xueqiuCfg:dict):
             fout.write(
                 fin.read().replace('{{title}}', tradeDateTxt+' 组合月收益%s%% 累计收益%s%%'%(xqPp['monthly_gain'],xqPp['total_gain'])).replace('{{rebalancing}}',latestDf).replace(
                     '{{position}}', holdingDf))
-    readText='策略组合月收益为百分之'+str(xqPp['monthly_gain'])+'，累计收益百分之'+str(xqPp['total_gain'])+'，当前持仓股票共'+str(len(holding))+'个，'+'，'.join(' '.join(x['stock_symbol'])+' '+x['stock_name']+'占百分之'+str(x['weight']) for x in xqPp['last']['holdings'])+'。调(tiao2)仓计划为卖出日涨幅最高个股并买入策略排名第一个股：'+'，'.join(' '.join(x['stock_symbol'])+' '+x['stock_name']+'从百分之'+str(x['prev_target_weight'])+'调(tiao2)到百分之'+str(x['target_weight']) for x in xqPp['latest']['rebalancing_histories'])+'，预计开盘时成交。'
+    readText='策略组合月收益为百分之'+str(xqPp['monthly_gain'])+'，累计收益百分之'+str(xqPp['total_gain'])+'，当前持仓股票共'+str(len(holding))+'个，'+'，'.join(' '.join(x['stock_symbol'])+' '+x['stock_name']+'占百分之'+str(x['weight']) for x in xqPp['last']['holdings'])+'。调仓计划为卖出日涨幅最高个股并买入策略排名第一个股：'+'，'.join(' '.join(x['stock_symbol'])+' '+x['stock_name']+'从百分之'+str(x['prev_target_weight'])+'调到百分之'+str(x['target_weight']) for x in xqPp['latest']['rebalancing_histories'])+'，预计开盘时成交。'
     print(readText)
     genVideo('http://127.0.0.1:5500/portfolio.html',readText,'Trade')
 
 def wencai(sentence:str,tradeDate:pd.DataFrame):
-    blacklist=['KIM']
+    blacklist=['KIM','ACAI','AMBG','CCYC','CHZQ','CNJG','CSJT','ECXJ','EZOO','HAFG','HJGP','WETG']
     df=crawl_data_from_wencai(sentence)
+    print(df)
     df=df.loc[~df['hqCode'].isin(blacklist)]
     print(df.columns)
-    df = df[['股票代码', '股票简称','美股@最新价', '美股@成交额', '美股@区间振幅', '美股@最新涨跌幅', 'hqCode']][:50]
+    df = df[['股票代码', '股票简称','美股@最新价', '美股@成交额', '美股@最新涨跌幅', 'hqCode']][:50]
     # df['美股@振幅'] = pd.to_numeric(df['美股@振幅'], errors='coerce')
     # df['美股@最新涨跌幅'] = pd.to_numeric(df['美股@最新涨跌幅'], errors='coerce')
     dfNewsLen = []
@@ -342,7 +263,7 @@ def wencai(sentence:str,tradeDate:pd.DataFrame):
         axis=1)
     renderHtml(dfH5,'../CMS/source/Quant/wc_us_%s.html' % tradeDate.day,tradeDate.strftime("%Y-%m-%d"))
     readText='策略思路讲解请查看往期视频，今日策略条件为：'+sentence+'。策略选出三个股票是：'
-    if not os.path.isfile('strategy.mp3'):
+    if not os.path.isfile('strategy.wav'):
         text2voice(readText,'strategy')
     if not os.path.isfile('strategy.mp4'):
         with open("Template/strategyTemp.xhtml", "r") as fin:
@@ -356,7 +277,6 @@ def wencai(sentence:str,tradeDate:pd.DataFrame):
     return symbols
 
 def combineFinal(symbols:list,tradeDate:pd.DataFrame):
-    symbols.reverse()
     for symbol in symbols:
         if os.path.isfile(FOLDER+symbol+'.mp4'):
             continue
@@ -373,6 +293,7 @@ def run(xConfig:dict,symbols=[]):
         symbols=wencai(xConfig['strategy'],tradeDate)[:3]
         trade(xConfig,symbols)
         genTradeVideo(tradeDate,xConfig)
+    symbols.reverse()
     combineFinal(symbols,tradeDate)
 
 
