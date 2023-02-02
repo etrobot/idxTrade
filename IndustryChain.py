9f1a6eb47deb1ed138a85a0292a99d8f6bb66319
franklin <etrobot@outlook.com>
2023-02-03 01:53:09 +0800

    industrychain html

--- IndustryChain.py
+++ IndustryChain.py
@@ -1,5 +1,4 @@
-import requests,json
-import time as t
+from QuotaUtilities import *
 
 if __name__=='__main__':
     FILEPATH='html/'
@@ -8,11 +7,39 @@ if __name__=='__main__':
     with open(FILEPATH+'industryChain.json', 'w', encoding='utf-8') as f:
         json.dump(json.loads(resp.text), f)
     resultList=json.loads(resp.text)['data']['items']
+    df=pd.DataFrame()
     for v in resultList:
         for id in v['items']:
             indUrl='https://m.lbkrs.com/api/forward/industrial/chain/id?id='+id['id']
             print(indUrl,v['name']+id['id']+id['name'])
-            indjson=requests.get(indUrl, headers={"user-agent": "Mozilla"}).text
+            indjtxt=requests.get(indUrl, headers={"user-agent": "Mozilla"}).text
+            indjson=json.loads(indjtxt)
+            for x in indjson['data']['items']:
+                for y in x['items']:
+                    idf=pd.DataFrame(y['stocks'])
+                    if len(idf.columns)==0:
+                        continue
+                    idf = idf.loc[idf['market'] == 'CN']
+                    idf['year_chg'] = pd.to_numeric(idf['year_chg'], errors='coerce')
+                    idf['month_chg'] = pd.to_numeric(idf['month_chg'], errors='coerce')
+                    idf['last_day_chg'] = pd.to_numeric(idf['last_day_chg'], errors='coerce')
+                    idf['Ind']=v['name']
+                    idf['Chain']='<a href="%s">%s</a>'%('https://app.longbridgehk.com/community/industrial-chains/'+id['id'],id['name'])
+                    idf['Part'] = y['name']
+                    idf['Part_pct'] = idf['month_chg'].mean()
+                    idf = idf.sort_values(by=['month_chg'], ascending=False)
+                    idf.reset_index(inplace=True)
+                    idf['sort']=idf.index
+                    df=df.append(idf)
             with open(FILEPATH + v['name']+id['id']+id['name'].replace('/','|')+'.json', 'w', encoding='utf-8') as f:
-                json.dump(json.loads(indjson), f)
+                json.dump(indjson, f)
+        # break
         t.sleep(10)
+    df=df.sort_values(by=['sort','Part_pct'],ascending=False)
+    df.drop_duplicates(subset='counter_id', keep='first', inplace=True)
+    df = df.sort_values(by=['Part_pct'], ascending=False)
+    df[['year_chg','month_chg','last_day_chg','Part_pct']] = df[['year_chg','month_chg','last_day_chg','Part_pct']].applymap("{0:.2%}".format)
+    df=df[['Ind', 'Chain',  'Part', 'Part_pct','market', 'counter_id','name', 'year_chg', 'month_chg','last_day_chg']]
+    df['counter_id'] = df['counter_id'].apply(
+        lambda x: '<a href="https://xueqiu.com/S/{stockCode}">{stockCode}</a>'.format(stockCode=(''.join(x.split('/')[1:])).replace('US','').replace('HK','')))
+    renderHtml(df,'../CMS/source/Quant/industryChain.html','产业链')
